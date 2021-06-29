999d5c809858077cfc018406f91eda91fbaacec6
Madevil <meowmeowandy@gmail.com>
2021-06-29 10:31:12 +0800

    StartupCameraRecall v1.0.0.0

---
+++ StartupCameraRecall/src/StartupCameraRecall.Core/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using static StartupCameraRecall.StartupCameraRecall;
+
+[assembly: AssemblyTitle(Name)]
+[assembly: AssemblyProduct(Name)]
+[assembly: AssemblyDescription(Name + " for " + Constants.GameName)]
+[assembly: AssemblyVersion(Version)]
+[assembly: AssemblyFileVersion(Version)]
---
+++ StartupCameraRecall/src/StartupCameraRecall.Core/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+using BepInEx;
+using BepInEx.Configuration;
+
+using KKAPI;
+using KKAPI.Studio.SaveLoad;
+
+namespace StartupCameraRecall
+{
+	[BepInPlugin(GUID, Name, Version)]
+	[BepInProcess(Constants.StudioProcessName)]
+	[BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+	[BepInIncompatibility("com.gebo.BepInEx.studioinitialcamera")]
+	public partial class StartupCameraRecall : BaseUnityPlugin
+	{
+		public const string GUID = "StartupCameraRecall";
+		public const string Name = "Startup Camera Recall";
+		public const string Version = "1.0.0.0";
+
+		private static ConfigEntry<KeyboardShortcut> Shortcut;
+		private static Studio.CameraControl.CameraData StartupCamera = new Studio.CameraControl.CameraData();
+
+		internal void Awake()
+		{
+			Shortcut = Config.Bind("Config", "Keyboard Shortcut", new KeyboardShortcut(KeyCode.BackQuote));
+			StudioSaveLoadApi.SceneLoad += OnSceneLoad;
+		}
+
+		private void Update()
+		{
+			if (Shortcut.Value.IsDown())
+				Studio.Studio.Instance.cameraCtrl.Import(StartupCamera);
+		}
+
+		private static void OnSceneLoad(object sender, SceneLoadEventArgs e)
+		{
+			if (e.Operation == SceneOperationKind.Load)
+				StartupCamera.Copy(Studio.Studio.Instance.sceneInfo.cameraSaveData);
+		}
+	}
+}
